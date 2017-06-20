584b5dacee7e3ca42abbdd2c06b26f89ead3c2d1
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-06-20 21:48:35 +0300

    Update mobilecenter_postclone.sh

--- mobilecenter_postclone.sh
+++ mobilecenter_postclone.sh
@@ -2,3 +2,5 @@
 echo "============================================="
 echo "======= MOBILECENTER POSTCLONE SCRIPT ======="
 echo "============================================="
+
+git clone https://github.com/pirate-test-prepost/prepost_android_java.git android_project
