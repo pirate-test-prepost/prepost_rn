53090c8dc6a505f8a228f645398028170605b2fb
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-06-20 16:10:35 +0300

    Update mobilecenter_postclone.sh

--- mobilecenter_postclone.sh
+++ mobilecenter_postclone.sh
@@ -2,3 +2,12 @@
 echo "============================================="
 echo "======= MOBILECENTER POSTCLONE SCRIPT ======="
 echo "============================================="
+
+echo "current branch is"
+echo $MOBILECENTER_BRANCH
+
+if [ $MOBILECENTER_BRANCH == "master" ]; then
+    # ...
+else
+    # ...
+fi
