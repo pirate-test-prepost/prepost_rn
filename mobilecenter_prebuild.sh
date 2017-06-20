d2afe8e302f64806104a5d0f852519a9e2a2184b
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-06-20 21:49:24 +0300

    Update mobilecenter_prebuild.sh

--- mobilecenter_prebuild.sh
+++ mobilecenter_prebuild.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "============================================="
 echo "======= MOBILECENTER PREBUILD SCRIPT ========"
+echo "====== SHOWS CONTENTS OF CLONED REPO ========"
 echo "============================================="
+
+ls ./android_project
