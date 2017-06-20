3545324ff4bbfcef971e2797b4a9336df9e108b0
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-06-20 16:27:36 +0300

    Update mobilecenter_postclone.sh

--- mobilecenter_postclone.sh
+++ mobilecenter_postclone.sh
@@ -6,6 +6,9 @@ echo "============================================="
 echo "current branch is"
 echo $MOBILECENTER_BRANCH
 
+echo $PATH
+echo $MODULENAME
+
 if [ $MOBILECENTER_BRANCH == "master" ]; then
     # ...
 else
