3259a3236ada9d6f40950ca78e37a89c5eed2c10
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-06-22 18:37:19 +0300

    Update mobile-center-postclone.sh

--- mobile-center-postclone.sh
+++ mobile-center-postclone.sh
@@ -2,3 +2,8 @@
 echo "============================================="
 echo "======= MOBILECENTER POSTCLONE SCRIPT ======="
 echo "============================================="
+
+echo "current branch is $BUILD_SOURCEBRANCHNAME"
+echo "android module name $moduleName"
+echo "android run tests $runTests"
+echo "android run lint $runLint"
