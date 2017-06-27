d69207429d0a29ffba54319dfbbe8702f953e5e4
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-06-27 18:00:24 +0300

    Update mobile-center-postclone.sh

--- mobile-center-postclone.sh
+++ mobile-center-postclone.sh
@@ -3,12 +3,4 @@ echo "============================================="
 echo "======= MOBILECENTER POSTCLONE SCRIPT ======="
 echo "============================================="
 
-echo "android module name $MODULENAME"
-echo "android run tests $RUNTESTS"
-echo "android run lint $RUNLINT"
-
-if ["$BUILD_SOURCEBRANCHNAME" == "master"]; then
-  echo "current branch is master"
-else
-  echo "current branch is $BUILD_SOURCEBRANCHNAME"
-fi
+printenv
