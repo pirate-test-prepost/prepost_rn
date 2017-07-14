1e9fa3e11fe4e361c1d58bac9e3dc9c1009d05e6
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-07-14 15:27:55 +0300

    Update mobile-center-pre-build.sh

--- mobile-center-pre-build.sh
+++ mobile-center-pre-build.sh
@@ -2,3 +2,5 @@
 echo "============================================="
 echo "======= MOBILECENTER PREBUILD SCRIPT ========"
 echo "============================================="
+
+echo "Using custom variable MOBILECENTER_CUSTOM_VAR = $MOBILECENTER_CUSTOM_VAR"
