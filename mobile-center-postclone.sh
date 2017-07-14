a489b4509331e8507af5324c5390b3c4d5e0cf7c
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-07-14 15:27:03 +0300

    custom var

--- mobile-center-postclone.sh
+++ mobile-center-postclone.sh
@@ -2,3 +2,6 @@
 echo "============================================="
 echo "======= MOBILECENTER POSTCLONE SCRIPT ======="
 echo "============================================="
+
+echo "Setting custom variable"
+echo "##vso[task.setvariable variable=MOBILECENTER_CUSTOM_VAR]crushed tomatoes"
