46b7ef5e3359f83df4fa269f77a44b25ef9e19aa
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-10-02 13:36:08 +0300

    Update mobile-center-post-clone.sh

--- mobile-center-post-clone.sh
+++ mobile-center-post-clone.sh
@@ -4,11 +4,11 @@ echo "======= MOBILECENTER POSTCLONE SCRIPT ======="
 echo "============================================="
 
 echo "name $username password $password"
-if [ "$username" == "test user name"]; then
+if [ "$username" == "test user name" ]; then
   echo "username is valid"
 fi
 
-if [ "$password" == "123"]; then
+if [ "$password" == "123" ]; then
   echo "password is valid"
 fi
 
